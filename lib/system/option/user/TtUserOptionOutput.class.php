1f7568d07756c0553751545d295f807dc9e5a210
Felix W <development@d1strict.de>
2021-01-21 16:02:38 +0100

    1.0.1
    +/- @ is not automatically removed.
    +/- Blank lines are only partially removed.
    +/- English language variable missing;

--- lib/system/option/user/TtUserOptionOutput.class.php
+++ lib/system/option/user/TtUserOptionOutput.class.php
@@ -16,12 +16,13 @@ class TtUserOptionOutput implements IUserOptionOutput {
 public function getOutput(User $user, UserOption $option, $value) {
 		if (empty($value)) return '';
 
-		$value = trim($value);
-		$link = 'https://tiktok.com/@' . $value;
+		$value = str_replace('@', "", $value);
+		$value = StringUtil::trim($value);
+		$link = 'https://www.tiktok.com/@' . $value;
 
 		$link = StringUtil::encodeHTML($link);
 		$value = StringUtil::encodeHTML($value);
-
+		
 		return '<a href="'.$link.'" class="externalURL"'.(EXTERNAL_LINK_REL_NOFOLLOW ? ' rel="nofollow"' : '').(EXTERNAL_LINK_TARGET_BLANK ? ' target="_blank"' : '').'>'.$value.'</a>';
 	}
 }
